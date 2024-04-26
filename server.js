3851e90581017ffea867bad6b901384ff9fb99fd
Ordoga <ordogga@gmail.com>
2024-04-26 09:47:13 +0300

    Added deploy site to CORS

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const corsOptions = {
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://localhost:3000",
+        "https://backend-miss-bug.onrender.com",
     ],
     credentials: true,
 }
